3a986ad4cffe8f9f4c501d245eb9a39f0b554116
MihaelRBX <145815987+MihaelRBX@users.noreply.github.com>
2023-11-06 15:23:34 -0300

    Add files via upload

--- projeto2.py
+++ projeto2.py
@@ -1,3 +1,27 @@
+def cadastrar_conta_corrente():
+    print("Você escolheu a opção para cadastro de conta corrente.")
+
+
+def depositar():
+    print("Você escolheu a opção de depositar.")
+
+
+def sacar():
+    print("Você escolheu a opção de sacar.")
+
+
+def consultar_saldo():
+    print("Você escolheu a opção para consulta de saldo.")
+
+
+def consultar_extrato():
+    print("Você escolheu a opção para consulta de extrato.")
+
+
+def finalizar():
+    print("Você escolheu a opção para finalizar o programa.")
+
+
 def imprimeEscolhas():
     print("MACK BANK - ESCOLHA UMA OPCÃO")
     print("   (1)   CADASTRAR CONTA CORRENTE")
@@ -6,10 +30,21 @@ def imprimeEscolhas():
     print("   (4)   CONSULTAR SALDO")
     print("   (5)   CONSULTAR EXTRATO")
     print("   (6)   FINALIZAR")
+    
     opcao = int(input("SUA OPCÃO: "))
+    
     while opcao < 1 or opcao > 6:
-        opcao = int(input("OPCÃO INVÁLIDA, INSIRA NOVAMENTE UMA OPCAO VÁLIDA: "))
-    return opcao
-
-
-imprimeEscolhas()
+        opcao = int(input("OPCÃO INVÁLIDA, INSIRA UMA OPCAO VÁLIDA: "))
+    
+    if opcao == 1:
+        cadastrar_conta_corrente()
+    elif opcao == 2:
+        depositar()
+    elif opcao == 3:
+        sacar()
+    elif opcao == 4:
+        consultar_saldo()
+    elif opcao == 5:
+        consultar_extrato()
+    elif opcao == 6:
+        finalizar()
