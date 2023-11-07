385076c1ad9235714b23399ae2eee70af2cf25bb
MihaelRBX <145815987+MihaelRBX@users.noreply.github.com>
2023-11-06 22:41:16 -0300

    Add files via upload

--- projeto2.py
+++ projeto2.py
@@ -1,5 +1,60 @@
+import random 
+import getpass
+
+cadastro_realizado = False
+
 def cadastrar_conta_corrente():
-    print("Você escolheu a opção para cadastro de conta corrente.")
+    global cadastro_realizado
+
+    if cadastro_realizado:
+        print("Você já realizou o cadastro uma vez. Não é permitido cadastrar mais de uma vez.")
+        return 
+    
+    print("MACK BANK – CADASTRO DE CONTA")
+    
+    numero_conta = random.randint(1000, 9999)
+    print("NÚMERO DA CONTA: ", numero_conta)
+    
+    nome_cliente = input("NOME DO CLIENTE: ")
+    while not nome_cliente:
+        print("Nome do cliente não pode estar em branco.")
+        nome_cliente = input("NOME DO CLIENTE: ")
+    
+    telefone_cliente = input("TELEFONE.......: ")
+    while not telefone_cliente:
+        print("Telefone não pode estar em branco.")
+        telefone_cliente = input("TELEFONE.......: ")
+    
+    email_cliente = input("EMAIL..........: ")
+    while not email_cliente:
+        print("Email não pode estar em branco.")
+        email_cliente = input("EMAIL..........: ")
+    
+    saldo_inicial = float(input("SALDO INICIAL...:R$ "))
+    while saldo_inicial < 1000:
+        print("O saldo inicial deve ser maior ou igual a R$1000.")
+        saldo_inicial = float(input("SALDO INICIAL...:R$ "))
+
+    limite_credito = float(input("LIMITE DE CRÉDITO: R$ "))
+    while limite_credito < 0:
+        print("O limite de crédito não pode ser menor que 0.")
+        limite_credito = float(input("LIMITE DE CRÉDITO: R$ "))
+
+    senha = getpass.getpass("SENHA..........: ")
+    while len(senha) != 6:
+        print("A SENHA DEVE SER COMPOSTA POR 6 CARACTERES.")
+        senha = getpass.getpass("SENHA..........: ")
+
+    confirmacao_senha = getpass.getpass("REPITA A SENHA...: ")
+    while confirmacao_senha != senha:
+        print("AS SENHAS INSERIDAS NÃO SÃO IGUAIS.")
+        confirmacao_senha = getpass.getpass("REPITA A SENHA...: ")
+    
+    cadastro_realizado = True
+
+    input("CADASTRO REALIZADO! PRESSIONE UMA TECLA PARA VOLTAR AO MENU...")
+
+
 
 
 def depositar():
@@ -48,3 +103,6 @@ def imprimeEscolhas():
         consultar_extrato()
     elif opcao == 6:
         finalizar()
+
+
+imprimeEscolhas()
